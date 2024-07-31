6dd4070c533d5e57cf4a5048f42493dcc6a1eccb
Marcos Moreira <marcoscurymoreira@gmail.com>
2024-07-31 17:10:07 -0300

    Adding typescript

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,7 +1,13 @@
 import styles from './Avatar.module.css'
 
+interface AvatarProps {
+  hasBorder?: boolean
+  src: string
+  alt?: string
+}
 
-export function Avatar({hasBorder = true, src}) {
+
+export function Avatar({hasBorder = true, src}: AvatarProps) {
 
   const border = hasBorder ? styles.avatarWithBorder : styles.avatar
   return (
--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,13 +1,31 @@
-import { useState } from 'react';
+import { ChangeEvent, FormEvent, useState } from 'react';
 import { Avatar } from './Avatar';
 import { Comment } from './Comment';
 import styles from './Post.module.css'
 import { format, formatDistanceToNow } from 'date-fns'
 import { ptBR } from 'date-fns/locale/pt-BR'
 
-export function Post({author, publishedAt, content}) {
+interface Author {
+  name: string
+  role: string
+  avatarUrl: string
+}
 
-  const [comments, setComments] = useState([])
+interface Content {
+  // type: 'paragraph' | 'link'
+  type: string
+  content: string
+}
+
+interface PostProps {
+  author: Author
+  publishedAt: Date
+  content: Content[]
+}
+
+export function Post({author, publishedAt, content}: PostProps) {
+
+  const [comments, setComments] = useState<string[]>([])
   const [newCommentText, setNewCommentText] = useState('')
 
   const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
@@ -19,17 +37,17 @@ export function Post({author, publishedAt, content}) {
     addSuffix: true,
   })
 
-  function handleCreateNewComment(event) {
+  function handleCreateNewComment(event: FormEvent) {
     event.preventDefault();
     setComments([...comments, newCommentText]);
     setNewCommentText('');
   }
 
-  function handleNewCommentChange(event) {
+  function handleNewCommentChange(event: ChangeEvent<HTMLTextAreaElement>) {
     setNewCommentText(event.target.value);
   }
 
-  function deleteComment(commentToDelete) {
+  function deleteComment(commentToDelete: string) {
     const commentsWithoutDeleteOne = comments.filter(comment => {
       return comment !== commentToDelete
     })
